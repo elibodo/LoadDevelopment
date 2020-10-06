5f0f15a21b98dc39dcb732e7a4dd80d6c3ff65e7
Eli Bodovinitz <ebodovinitz@gmail.com>
2020-10-06 16:48:32 -0400

    Created main class

---
+++ LoadDevelopment/LoadDevelopment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LoadDevelopment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
