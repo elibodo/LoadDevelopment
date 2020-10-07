db89ff0bec6cd503562a51d3ab7b81a8ff79001b
Eli Bodovinitz <ebodovinitz@gmail.com>
2020-10-06 21:01:29 -0400

    Added 'Spread' and 'SDev' methods for analyzing the data that the user inputs.

--- LoadDevelopment/LoadDevelopment/Program.cs
+++ LoadDevelopment/LoadDevelopment/Program.cs
@@ -6,7 +6,33 @@ namespace LoadDevelopment
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string input = "";
+            Console.WriteLine("This app will help the user find a load for reloading rifle ammunition.");
+            Console.WriteLine("\n\nTo find the lowest spread and a good starting point for a new load type 'SPREAD'");
+            Console.WriteLine("\n\nTo find the best standard deviation using 5 shot groups type 'SDEV'.\n");
+            while (input != "SPREAD" || input != "SDEV")
+            {
+                input = Console.ReadLine().ToUpper();
+
+                if (input == "SPREAD")
+                {
+                    Spread();
+                }
+                else if (input == "SDEV")
+                {
+                    SDev();
+                }
+            }
+        }
+
+        public static void Spread()
+        {
+            Console.WriteLine("spread");
+        }
+
+        public static void SDev()
+        {
+            Console.WriteLine("sdev");
         }
     }
 }
