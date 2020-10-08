d0992d122682a319b9b4a204f2feed68584e1261
Eli Bodovinitz <ebodovinitz@gmail.com>
2020-10-08 09:29:11 -0400

    Added an algorithm that finds the best spread from an array of velocities. Then prints the charge associated with those two velocities.

--- LoadDevelopment/LoadDevelopment/Program.cs
+++ LoadDevelopment/LoadDevelopment/Program.cs
@@ -27,7 +27,42 @@ namespace LoadDevelopment
 
         public static void Spread()
         {
-            Console.WriteLine("spread");
+            Console.WriteLine("Input velocities from a 20 shot test where there is 2 shots at each charge.");
+            Console.WriteLine("Press enter between each velocity.");
+            int[] shots = new int[20];
+            int charge = 1;
+            for (int i = 0; i < shots.Length; i++)
+            {   
+                string velocity;
+                Console.Write("Charge " + charge + ": ");
+                velocity = Console.ReadLine();
+                shots[i] = Convert.ToInt32(velocity);
+                if (i % 2 == 1)
+                {
+                    charge++;
+                    Console.WriteLine();
+                }
+            }
+            int bestCharge = (FindSpread(shots)/2) + 1;
+            Console.WriteLine("Best spread was with charge: " + bestCharge);
+
+        }
+
+        public static int FindSpread(int[] shot)
+        {
+            int bestCharge = int.MaxValue;
+            int best = int.MaxValue;
+            for (int i = 0; i < shot.Length-1; i+=2)
+            {
+                int j = i + 1;
+                int spread = Math.Abs(shot[i] - shot[j]);
+                if (spread < best)
+                {
+                    best = spread;
+                    bestCharge = i;
+                }
+            }
+            return bestCharge;
         }
 
         public static void SDev()
